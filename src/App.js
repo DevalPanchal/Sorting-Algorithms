44c2b068ef7048e795b0beec56385972f160ddd0
Deval Panchal <devalp2401@gmail.com>
2022-01-19 14:28:24 -0500

    Fixing commit errors

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import { SortingVisualizer } from "./Components/SortingVisualizer";
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import "./stylesheets/styles.css";
 
 function App() {
   	return (
   	  	<div className="App">
-		    <Router>
-		    		<Routes>
-				    <Route exact path="/" element={ <SortingVisualizer /> } />
-				</Routes>
-		    </Router>
+			<SortingVisualizer />
   	  	</div>
   	);
 }
